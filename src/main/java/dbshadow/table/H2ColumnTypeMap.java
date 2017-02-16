1532061f6f0ca9e3afea99688ff21f4ae6a1117f
dthompson <dthompson@sofi.org>
2017-02-16 11:34:48 -0700

    add new typemap for h2.

---
+++ src/main/java/dbshadow/table/H2ColumnTypeMap.java
@@ -0,0 +1,38 @@
+package dbshadow.table;
+
+public class H2ColumnTypeMap implements ColumnTypeMap {
+    public String to(boolean pkey, String colType, DBType destDbType, int precision, int scale) {
+        String newType = colType;
+
+        if (destDbType == DBType.Oracle) {
+            if (colType.equals("INTEGER"))
+                newType = "NUMBER";
+        } else if (destDbType == DBType.MYSQL) {
+            if (colType.equals("CHARACTER"))
+                newType = "VARCHAR";
+        }
+
+        // These types don't require precision or scale.
+        if (newType.equals("INTEGER") ||
+            newType.equals("TIME") ||
+            newType.equals("DATE") ||
+            newType.equals("TIMESTAMP")) {
+
+            if (pkey && destDbType != DBType.HSQL)
+                newType += " PRIMARY KEY";
+            return newType;
+        }
+
+        if (precision <= 0 && scale <= 0)
+            ;
+        else if (precision > 0 && scale > 0)
+            newType += "(" + precision + ","	+ scale + ")";
+        else
+            newType += "(" + precision + ")";
+
+
+        if (pkey && destDbType != DBType.HSQL)
+            newType += " PRIMARY KEY";
+        return newType;
+    }
+}
